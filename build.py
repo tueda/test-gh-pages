625fe38a4c28e813ac24684a809f56437fcd2331
Takahiro Ueda <takahiro.ueda@yahoo.com>
2024-04-01 00:39:51 +0900

    feat: initial commit

---
+++ build.py
@@ -0,0 +1,18 @@
+with open("index.html") as f:
+    f.write(
+        """\
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>テスト</title>
+</head>
+
+<body>
+    <p>これはテストです。</p>
+</body>
+
+</html>
+"""
+    )
